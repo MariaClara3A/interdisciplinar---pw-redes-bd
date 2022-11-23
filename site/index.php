629b60b497790e68bbf0a4b7d985419ae485432e
Maria Clara <clwramif@gmail.com>
2022-11-23 16:05:19 -0400

    projeto completo

---
+++ site/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '_parts/_linkCSS.php'; ?>
+    <title>Usuários</title>
+</head>
+
+<body>
+    <?php include_once '_parts/_header.php'; ?>
+    <?php include '_parts/_linkJS.php'; ?>
+    <h1> KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK </h1>
+</body>
+
+</html>
